138c42a3a4cd7cf57463e4eabe0d33af0bee7d4e
edfaviel <edfaviel_jr@hotmail.com>
2018-09-19 14:48:48 -0500

    upload files

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow} = require('electron')
+
+
+  function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({ width: 500, height: 900, center: true, resizable: false})
+  
+    // and load the index.html of the app.
+    win.loadFile('index.html')
+
+  }
+  
+
+  app.on('ready', createWindow)
