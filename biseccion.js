23c8062be53c0a6e911740be0a83ce3b70c2f617
edfaviel <edfaviel_jr@hotmail.com>
2018-09-20 13:13:25 -0500

    changes

--- biseccion.js
+++ biseccion.js
@@ -1,7 +1,7 @@
 function biseccion (){
     var  form = document.forms["biseccion"].elements[0].value;
     var  formula =document.getElementById("formula").value;
-    var a= document.getElementById("a").value;
+    var a = document.getElementById("a").value;
     var b = document.getElementById("b").value;
 
     
@@ -10,11 +10,13 @@ function biseccion (){
         alert('inserte por lo menos una suma o resta');
     }
     else{
+        var reemplazoX = {};
         for(i=0;i<separar.length;i++){
-            
+            reemplazoX[i] = separar.replace('x', a)
         
 
         }
+        
         document.getElementById("mostrar").innerHTML='<textarea name="mostrar" rows="5" cols="30">Por formula: '+formula+' \
             <br>Por separar: '+separar+' \
             <br>Por a: '+a+' \
